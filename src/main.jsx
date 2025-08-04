09685d0b8f253f2d279b235a573f8db40a4e6a96
munhozvinicius <munhoz.vinicius@gmail.com>
2025-08-04 17:17:32 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import './index.css'
+
 import LoginPage from './pages/LoginPage.jsx'
 import AdminLoginAppearance from './pages/admin/AdminLoginAppearance.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        {/* Home redireciona para /login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/admin/aparencia/login" element={<AdminLoginAppearance />} />
+        <Route
+          path="/admin/aparencia/login"
+          element={<AdminLoginAppearance />}
+        />
+        {/* qualquer rota desconhecida -> /login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
