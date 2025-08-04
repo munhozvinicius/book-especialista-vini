befbc4834d9051bb1fd732d7da4317df4b678cf9
munhozvinicius <munhoz.vinicius@gmail.com>
2025-08-04 14:44:52 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,2 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-export const supabase = createClient('https://blfntsyompcpoepvqqmv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsZm50c3lvbXBjcG9lcHZxcW12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzMDY2MDksImV4cCI6MjA2OTg4MjYwOX0.1EOgw8OXtZ0RKCLHcTOhS9UK-mIFPKCx6FLGo0aFKhw')
+
+const url = import.meta.env.VITE_SUPABASE_URL
+const anon = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(url, anon)
