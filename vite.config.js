2e275e855ab2a5f765ab21e0aad12fee4ab65d01
munhozvinicius <munhoz.vinicius@gmail.com>
2025-08-04 15:07:53 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,8 @@ import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
   },
-  // força o Vite a pré-bundlar o pacote do Supabase, evitando o erro de resolução
-  optimizeDeps: {
-    include: ['@supabase/supabase-js']
-  },
-  ssr: {
-    noExternal: ['@supabase/supabase-js']
-  }
+  optimizeDeps: { include: ['@supabase/supabase-js'] },
+  ssr: { noExternal: ['@supabase/supabase-js'] }
 })
