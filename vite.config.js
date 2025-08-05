f7afb0b382da54a5c773fa41cfb0af8b183bcffb
munhozvinicius <munhoz.vinicius@gmail.com>
2025-08-04 22:13:49 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   resolve: {
-    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } ,
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-  optimizeDeps: { include: ['@supabase/supabase-js'] },
-  ssr: { noExternal: ['@supabase/supabase-js'] }
 })
