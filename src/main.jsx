57ebf4bc63ff40f1a6cf0fc5cd05055740aa1959
munhozvinicius <munhoz.vinicius@gmail.com>
2025-08-04 21:18:03 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route
-          path="/dashboard"
+          path="/*"
           element={
             <ProtectedRoute>
-              <Dashboard />
+              <App />
             </ProtectedRoute>
           }
         />
-        <Route path="*" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
